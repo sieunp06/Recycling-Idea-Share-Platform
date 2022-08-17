b23269f165156e0cd25749a1cae2c1370fa958c3
Sieun Park <psieun0606@gmail.com>
2022-08-17 23:26:09 +0900

    🎨 myPage UI

--- lib/myPage.dart
+++ lib/myPage.dart
@@ -1,13 +1,47 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class myPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        "my Page",
-        style: TextStyle(fontSize: 40),
-      ),
+    return Column(
+      children: [
+        Stack(
+          children: [
+            Container(
+              color: Color(0xff5b6354),
+              width: 360.w,
+              height: 250.h
+            ),
+            Positioned(
+              top: 25.0.h,
+              left: 25.0.w,
+              child: Text(
+                "MY",
+                softWrap: true,
+                overflow: TextOverflow.fade,
+                style: TextStyle(
+                    fontSize: 30.sp,
+                    color: Colors.white,
+                    // fontWeight: FontWeight.bold,
+                    fontFamily: 'namumGothic', fontWeight: FontWeight.w700
+                ),
+              ),
+            ),
+            // Todo. 원형 이미지로 변경하기
+            Positioned(
+              top: 80.h,
+              left: 25.w,
+              child: Icon(
+                Icons.account_circle_rounded,
+                color: Colors.white,
+                size: 120.h,
+              ),
+            )
+          ],
+        )
+      ],
     );
   }
 }
