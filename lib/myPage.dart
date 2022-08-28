2d91bc669518453328bf18bd6aa025938dd378bc
Sieun Park <psieun0606@gmail.com>
2022-08-28 20:00:39 +0900

    🎨 Edit myPage UI

--- lib/myPage.dart
+++ lib/myPage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:gigo/login.dart';
 
 class myPage extends StatelessWidget {
   @override
@@ -40,9 +41,71 @@ class myPage extends StatelessWidget {
               ),
             ),
             Positioned(
+              top: 125.h,
+              left: 145.w,
+              // 로그인 안했을 때 마이 페이지 화면
+              child: GestureDetector(
+                child: Text(
+                  '로그인',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 25.sp,
+                    fontFamily: 'namumGothic', fontWeight: FontWeight.w700
+                  ),
+                ),
+                onTap: (){
+                  print('로그인 클릭');
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => Login()),
+                  );
+                }
+              ),
+            ),
+            /*
+            Positioned(
+              top: 145.h,
+              left: 145.w,
               child: Text(
-                "사용자 이름"
-              )
+                "레벨",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20.sp,
+                    fontFamily: 'namumGothic', fontWeight: FontWeight.w700
+                ),
+              ),
+            ),
+            */
+          ],
+        ),
+        Column(
+          children: <Widget>[
+            ListView(
+              physics: NeverScrollableScrollPhysics(),
+              shrinkWrap: true,
+              children: [
+                ListTile(
+                  title: Text(
+                    '고객센터',
+                    style: TextStyle(
+                      fontSize: 15.sp,
+                    )
+                  ),
+                  trailing: Icon(Icons.navigate_next),
+                  //todo. 고객센터 클릭시 내용 추가
+                  onTap: () {
+                    print('고객센터 클릭');
+                  },
+                ),
+                ListTile(
+                  title: Text('공지사항'),
+                  //todo. 공지사항 클릭시 내용 추가
+                  trailing: Icon(Icons.navigate_next),
+                  onTap: () {
+                    print('공지사항 클릭');
+                  },
+                ),
+              ]
             )
           ],
         )
